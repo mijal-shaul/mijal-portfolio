61e21f0f9fde1d1442b83cd2b4cc5c7a61b98cd7
Daniel <dshaul@tactech.cl>
2023-12-12 12:23:47 -0300

    use apos

--- components/about/Intro.jsx
+++ components/about/Intro.jsx
@@ -9,10 +9,10 @@ const Intro = () => {
           <div className="col-span-12 space-y-2.5">
             <div className="lg:mr-16">
               <p className="text-gray-lite  dark:text-color-910 leading-7">
-              As an experienced UX/UI designer, I have been involved in numerous projects where I have successfully created intuitive and visually appealing interfaces that enhance user experiences. Through my expertise in Figma, I have collaborated closely with cross-functional teams to create wireframes, prototypes, and interactive designs that align with business goals and user needs.
+                As an experienced UX/UI designer, I have been involved in numerous projects where I have successfully created intuitive and visually appealing interfaces that enhance user experiences. Through my expertise in Figma, I have collaborated closely with cross-functional teams to create wireframes, prototypes, and interactive designs that align with business goals and user needs.
               </p>
               <p className="text-gray-lite leading-7 mt-2.5 dark:text-color-910">
-              With my extensive experience in UX/UI design and proficiency in Figma, along with a solid foundation in HTML and CSS, I am confident that I can contribute to your team's success.
+                With my extensive experience in UX/UI design and proficiency in Figma, along with a solid foundation in HTML and CSS, I am confident that I can contribute to your team&apos;s success.
               </p>
             </div>
           </div>
